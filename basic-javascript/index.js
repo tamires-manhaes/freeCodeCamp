8913046ef05cbca397eeee2113a30308a3ca6e3d
Tamires <tamiresmanhaes4@gmail.com>
2020-04-18 00:26:39 -0300

    iniciando primeiro modulo do curso de js

---
+++ basic-javascript/index.js
@@ -0,0 +1,17 @@
+// comentario
+
+/*
+diferentes tipos de dados:
+- undefined 
+- null
+- boolean 
+- string
+- symbol 
+- number
+- object
+ */
+
+// exemplo de declaração de variável:
+var myName = 'Tamires';
+
+console.log("meu nome é " + myName);
